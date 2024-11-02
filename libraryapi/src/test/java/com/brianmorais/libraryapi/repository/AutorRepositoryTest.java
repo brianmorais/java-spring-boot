4bcc7b38a9b0286737fb3a5296f25467d229ffb6
Brian Leonardo de Morais <morais.brian360@gmail.com>
2024-11-02 14:22:53 -0300

    Aula 58

--- libraryapi/src/test/java/com/brianmorais/libraryapi/repository/AutorRepositoryTest.java
+++ libraryapi/src/test/java/com/brianmorais/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,5 +37,28 @@ public class AutorRepositoryTest {
             encontrado.setDataNascimento(LocalDate.of(1960, 1, 30));
             repository.save(encontrado);
         }
+    }
+
+    @Test
+    public void listarTest() {
+        List<Autor> lista = repository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest() {
+        System.out.println("Contagem de autores" + repository.count());
+    }
+
+    @Test
+    public void deletePorIdTest() {
+        var id = UUID.fromString("d75e4b4a-9d17-4710-8f10-51127e257c13");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest() {
+        var id = UUID.fromString("d75e4b4a-9d17-4710-8f10-51127e257c13");
+        repository.findById(id).ifPresent(autor -> repository.delete(autor));
     }
 }
